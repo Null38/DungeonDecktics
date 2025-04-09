5f096ce6bdd114b77276ab489541dc7f93fa227c
Null38 <62232764+Null38@users.noreply.github.com>
2025-04-09 20:44:21 +0900

    Controller Init

--- Assets/C#/Controller.cs
+++ Assets/C#/Controller.cs
@@ -1,27 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
-
-[System.Serializable]
-public class Node
+abstract public class Controller : MonoBehaviour
 {
-    public Node(bool _isWall, int _x, int _y) { isWall = _isWall; x = _x; y = _y; }
-
-    public bool isWall;
-    public Node ParentNode;
-
-    // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
-    public int x, y, G, H;
-    public int F { get { return G + H; } }
-}
-
-
-static public class PathFinder
-{
-    public static List<Node> PathFinding(Vector2Int startPos , Vector2Int endPos)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        throw new NotImplementedException();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
