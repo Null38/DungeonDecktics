1e86f0c2381ce156afdbb3ea5d9360a2771aafd3
kbhj6789 <kbhj6789@naver.com>
2025-05-14 23:58:53 +0900

    a

--- Assets/ScriptableObject/Script/Card/AttackCardObject.cs
+++ Assets/ScriptableObject/Script/Card/AttackCardObject.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "AttackCardObject", menuName = "Scriptable Objects/Card/AttackCard")]
+[CreateAssetMenu(fileName = "AttackCardObject", menuName = "ScriptableObjects/Card/AttackCard")]
 public class AttackCardObject : CardObject
 {
 
     public int damage;
-    public int test;
-    public int var;
-    public int ul;
 
     public override void UseCard()
     {
@@ -16,6 +13,6 @@ public class AttackCardObject : CardObject
 
     public override string FormatDescription()
     {
-        return string.Format(description, damage, test, var, ul);
+        return string.Format(description, damage);
     }
 }
