a581f99d81d890ede139efebef42b477a1d4308e
Null38 <62232764+Null38@users.noreply.github.com>
2025-05-17 01:25:43 +0900

    Coroutine Remove

--- Assets/Script/Managers/DataManager.cs
+++ Assets/Script/Managers/DataManager.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class DataManager
 {
-    public static Vector2Int MapSize = new Vector2Int(15, 9);
+    public static Vector2Int MapSize = new Vector2Int(15, 9);//맵 생성과 연동 해야함. || 지금생각해보니 이거 왜 여따뒀지 맵으로 옮기기
+    public static GameObject player; // 플레이어 오브젝트
+    public static List<GameObject> enemy = new(); // 적 오브젝트 || 이거 적 클래스로 옮겨야겠다.
 }
