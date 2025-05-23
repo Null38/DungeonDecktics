f8222dfa127e1dbd8806d279b6f49e9794fcddcf
Null38 <62232764+Null38@users.noreply.github.com>
2025-05-24 03:05:28 +0900

    wip

---
+++ Assets/Script/Managers/UI/InGameUiManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUiManager : MonoBehaviour
+{
+    [SerializeField]
+    Slider HpBar;
+    [SerializeField]
+    Slider ShildBar;
+    [SerializeField]
+    Button MenuButton;
+    [SerializeField]
+    GameObject Hand;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
