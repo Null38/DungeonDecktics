538738425ca9bb8678669ca0189bd0550b17cebf
Null38 <62232764+Null38@users.noreply.github.com>
2025-05-24 03:22:09 +0900

    fix

--- Assets/Script/Managers/OptionManager.cs
+++ Assets/Script/Managers/OptionManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class OptionManager : MonoBehaviour
 {
-    [Header("UI 패널")]
-    public GameObject optionPanel;
 
     [Header("Audio Mixer")]
     public AudioMixer audioMixer;
@@ -16,25 +14,9 @@ public class OptionManager : MonoBehaviour
     public Slider bgmVolumeSlider;
     public Slider sfxVolumeSlider;
 
-    // 옵션창 열기는 작동하는지 확인하기 위해 임시로 구현해뒀음. 유니티에선 삭제하고 코드에선 주석처리 해두겠음.
-    //public void OpenOption()
-    //{
-    //    if (optionPanel == null)
-    //        throw new System.Exception("optionPanel이 연결 안 됨");
-    //    optionPanel.SetActive(true);
-    //    InitializeOptionUI();
-    //}
-
-    // 옵션창 닫기
-    public void CloseOption()
-    {
-        if (optionPanel == null)
-            throw new System.Exception("optionPanel이 연결 안 됨");
-        optionPanel.SetActive(false);
-    }
 
     // 옵션창 열릴 때 슬라이더 값 초기화
-    public void InitializeOptionUI()
+    public void Awake()
     {
         if (audioMixer == null)
             throw new System.Exception("audioMixer 연결 안 됨");
@@ -56,6 +38,13 @@ public class OptionManager : MonoBehaviour
         sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
     }
     // 볼륨 조절 부분 제대로 작동하는지 소리 넣어봐야 할 듯.
+
+    // 옵션창 닫기
+    public void CloseOption()
+    {
+        SceneManager.UnloadSceneAsync("OptionScene");
+    }
+    
     // 마스터 볼륨 조절
     public void SetMasterVolume(float value)
     {
@@ -72,12 +61,5 @@ public class OptionManager : MonoBehaviour
     public void SetSFXVolume(float value)
     {
         audioMixer.SetFloat("SFXVolume", value);
-    }
-
-    // 게임 종료
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("게임 종료");
     }
 }
