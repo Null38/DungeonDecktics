d35c64e191dc615a254e02f0291d01d8efc75e8c
Null38 <62232764+Null38@users.noreply.github.com>
2025-05-23 15:27:35 +0900

    sizeFix

--- Assets/Script/ScriptableObject/DeckObject.cs
+++ Assets/Script/ScriptableObject/DeckObject.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "DeckData", menuName = "Scriptable Objects/Deck", order = 1)]
 public class DeckObject : ScriptableObject
 {
-    
+    [SerializeField]
+    private List<CardObjectBase> cards;
 }
