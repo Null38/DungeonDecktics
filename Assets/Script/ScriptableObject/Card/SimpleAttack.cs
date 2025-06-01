eb873b4f4dea06ccaceb3cba82f6323a87322cda
Null38 <62232764+Null38@users.noreply.github.com>
2025-06-01 21:10:06 +0900

    attack implant

--- Assets/Script/ScriptableObject/Card/SimpleAttack.cs
+++ Assets/Script/ScriptableObject/Card/SimpleAttack.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class SimpleAttack : CardBase
 {
     [Tooltip("이 배열 크기를 초과하게 설명창에 넣지 마세요.\n오류납니다.\n단, 마지막에는 데미지 합산이 들어가니 {배열길이}까지 가능합니다.")]
-    public int[] damage;
+    public int[] damages;
 
     public override string FormatDescription()
     {
         int total = 0;
 
-        object[] formatArray = new object[damage.Length + 1];
+        object[] formatArray = new object[damages.Length + 1];
 
-        for (int i = 0; i < damage.Length; i++)
+        for (int i = 0; i < damages.Length; i++)
         {
-            formatArray[i] = damage[i];
-            total += damage[i];
+            formatArray[i] = damages[i];
+            total += damages[i];
         }
-        formatArray[damage.Length] = total;
+        formatArray[damages.Length] = total;
 
         return string.Format(description, formatArray);
     }
@@ -26,11 +26,16 @@ public class SimpleAttack : CardBase
     public override string Upgrad()
     {
         throw new System.NotImplementedException();
+        isUpgraded = true;
     }
 
-    public override void UseCard(Controller user)
+    public override void UseCard(Controller target)
     {
-        throw new System.NotImplementedException();
-        isUpgraded = true;
+        foreach (var damage in damages)
+        {
+            Debug.Log(target);
+            target.info.currentHp -= damage;
+        }
+        Debug.Log("damaged");
     }
 }
