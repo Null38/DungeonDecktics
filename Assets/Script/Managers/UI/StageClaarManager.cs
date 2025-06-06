aa18cea81a7265a8196448d35fc5677c5dec05d8
Null38 <62232764+Null38@users.noreply.github.com>
2025-06-06 23:04:15 +0900

    Ready to implant

--- Assets/Script/Managers/UI/StageClaarManager.cs
+++ Assets/Script/Managers/UI/StageClaarManager.cs
@@ -5,23 +5,33 @@ using UnityEngine.UI;
 public class StageClearSceneUIManager : MonoBehaviour
 {
     [SerializeField] private Button buttonNextStage;
-        
-    [SerializeField] private int nextStageIndex = 1;
+
+    [SerializeField] private Button buttonPass;
+    [SerializeField] private Button buttonChange;
+
+    [SerializeField] private Image CurrEq;
+    [SerializeField] private Image newEq;
+
+    [SerializeField] private GameObject EquipmentChangeTap;
+
+
 
     private void Awake()
     {
-        if (buttonNextStage == null)
-        {
-            return;
-        }
-
         buttonNextStage.onClick.AddListener(OnNextStageClicked);
+        buttonPass.onClick.AddListener(() => EquipmentChangeTap.SetActive(false));
+        buttonChange.onClick.AddListener(SetNewEq);
+
+        //CurrEq.sprite = GameManager.Instance.무슨무슨 장비
+        //newEq.sprite = 무슨무슨 새로운 장비
+        Debug.LogWarning("이 문구 보이면 구현하라 하는거임");
     }
 
-    private void OnDestroy()
+    private void SetNewEq()
     {
-        if (buttonNextStage != null)
-            buttonNextStage.onClick.RemoveListener(OnNextStageClicked);
+        EquipmentChangeTap.SetActive(false);
+        //DumbAssSave.eqSave[장] = 비;
+        Debug.LogWarning("이 문구 보이면 구현하라 하는거임");
     }
 
     private void OnNextStageClicked()
